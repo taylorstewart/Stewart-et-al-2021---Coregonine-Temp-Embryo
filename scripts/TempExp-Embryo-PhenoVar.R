def21f4f71c58d7cb8bf61cd3be1b0aa55511807
taylorstewart <taylor.stewart@uvm.edu>
2020-12-23 11:03:52 -0500

    update phenoVar

--- scripts/TempExp-Embryo-PhenoVar.R
+++ scripts/TempExp-Embryo-PhenoVar.R
@@ -155,39 +155,6 @@ temp <- data.frame(group = c("LK-Whitefish", "LK-Whitefish", "LK-Whitefish", "LK
                                            ordered = TRUE, levels = c("Coldest", "Cold", "Warm", "Warmest")))
 
 
-#### CALCULATE BOOTSTRAPPED ERROR ESTIMATES ------------------------------------------------------
-
-phenoVar.ES.boot <- read.csv("data/PhenoVar_Bootstrap/PhenoVar_ES_Var_Boot.csv")
-phenoVar.DPF.boot <- read.csv("data/PhenoVar_Bootstrap/PhenoVar_DPF_Var_Boot.csv")
-phenoVar.ADD.boot <- read.csv("data/PhenoVar_Bootstrap/PhenoVar_ADD_Var_Boot.csv")
-
-phenoVar.embryo.boot <- bind_rows(phenoVar.ES.boot, phenoVar.DPF.boot, phenoVar.ADD.boot) %>% 
-  mutate(temperature = as.numeric(gsub("_", ".", gsub("C", "", substr(group, nchar(group)-3, nchar(group))))),
-         group = gsub("_", "-", substr(group, 1, nchar(group)-5)),
-         dam.perc = (dam/total)*100,
-         sire.perc = (sire/total)*100,
-         dam.sire.perc = (dam.sire/total)*100,
-         residual.perc = (residual/total)*100) %>% 
-  filter(group != "LK-Whitefish") %>% 
-  left_join(temp) %>% 
-  group_by(group, temperature, temp.treatment) %>% 
-  summarize(dam.se = sd(dam.perc),
-            sire.se = sd(sire.perc),
-            dam.sire.se = sd(dam.sire.perc),
-            residual.se = sd(residual.perc)) %>% 
-  pivot_longer(4:7, names_to = "component", values_to = "error") %>% 
-  mutate(component = factor(component, ordered = TRUE,
-                            levels = c("dam.se", "sire.se", "dam.sire.se", "residual.se"),
-                            labels = c("Dam", "Sire", "Dam.Sire", "Error")),
-         component.trt = factor(interaction(component, temp.treatment), ordered = TRUE,
-                                levels = c("Dam.Coldest", "Dam.Cold", "Dam.Warm", "Dam.Warmest",
-                                           "Sire.Coldest", "Sire.Cold", "Sire.Warm", "Sire.Warmest",
-                                           "Dam.Sire.Coldest", "Dam.Sire.Cold", "Dam.Sire.Warm", "Dam.Sire.Warmest",
-                                           "Error.Coldest", "Error.Cold", "Error.Warm", "Error.Warmest")),
-         group = factor(group, ordered = TRUE, 
-                        levels = c("LK-Vendace", "LS-Cisco", "LO-Cisco")))
-         
-
 #### CALCUALTE MEAN VARIANCE ACROSS TEMPERATURES -------------------------------------------------
 
 phenoVar.embryo.mean <- bind_rows(phenoVar.survival.obs, phenoVar.dpf.obs, phenoVar.ADD.obs) %>% 
@@ -222,7 +189,7 @@ phenoVar.embryo.error <- bind_rows(phenoVar.survival.obs, phenoVar.dpf.obs, phen
                         levels = c("LK-Vendace", "LS-Cisco", "LO-Cisco")))
 
 
-# JOIN MEAN AND ERROR -------------------------------------------------------------------------
+#### JOIN MEAN AND ERROR -------------------------------------------------------------------------
 
 phenoVar.embryo.all <- left_join(phenoVar.embryo.mean, phenoVar.embryo.error)
 
